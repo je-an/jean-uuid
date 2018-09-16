75ca0a35d48fe355be4c0803ca0314f119d9ef64
je-an <mail@jeremiasandris.de>
2018-09-16 21:35:58 +0200

    additional improvements

--- src/Uuid.js
+++ src/Uuid.js
@@ -7,15 +7,14 @@ define(["Byte"], function (Byte) {
         Byte: Byte,
         /** @returns {String} - the generated uuid */
         generate: function () {
-            var bin = [], d = [], bytes = [];
-            var j, i;
+            var binaryValues = [], bytes = [], i, ;
             //Create 128 random bits
-            for (var i = 0; i < 128; i++) {
-                bin.push(Math.round(Math.random()));
+            for (i = 0; i < 128; i++) {
+                binaryValues.push(Math.round(Math.random()));
             }
             // Create 16 Byte objects from the 128 random bits
-            for (var i = 0; i < 128; i = i + 8) {
-                bytes.push(new Byte(bin.slice(i, i + 8)));
+            for (i = 0; i < 128; i = i + 8) {
+                bytes.push(new Byte(binaryValues.slice(i, i + 8)));
             }
             // To put in the version, take the 7th byte and perform an and operation using 0x0f, 
             // followed by an or operation with 0x40. 
@@ -27,7 +26,7 @@ define(["Byte"], function (Byte) {
             var ninthByte = bytes[8];
             ninthByte.doAndOperation([0, 0, 1, 1, 1, 1, 1, 1]);
             ninthByte.doOrOperation([1, 0, 0, 0, 0, 0, 0, 0]);
-
+            // Create the uuid string
             return bytes[0].hexString + bytes[1].hexString + bytes[2].hexString + bytes[3].hexString + "-" +
                 bytes[4].hexString + bytes[5].hexString + "-" + bytes[6].hexString + bytes[7].hexString + "-" +
                 bytes[8].hexString + bytes[9].hexString + "-" + bytes[10].hexString + bytes[11].hexString +
